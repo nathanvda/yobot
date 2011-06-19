63a771f522304cc802b0a3489b46cca9e793fccc
Alexander Lang <alex@upstre.am>
2011-06-19 11:50:05 +0200

    type, load bundler

--- lib/yobot.rb
+++ lib/yobot.rb
@@ -5,25 +5,11 @@ end
 
 $LOAD_PATH.unshift File.dirname(__FILE__)
 
+require "rubygems"
+require "bundler/setup"
+
 require 'firering'
-require 'JSON'
+require 'json'
 require 'yobot/bot'
 require 'yobot/behaviors/ping_pong'
 require 'yobot/behaviors/dict'
-
-EM.run do
-  conn.authenticate do |user|
-    conn.rooms do |rooms|
-      rooms.each do |room|
-        room.text('Hello World') {}
-        room.stream do |message|
-          if message.type == 'TextMessage'
-            bot.received_message room, message.body
-          end
-        end
-      end
-    end
-  end
-
-  trap("INT") { EM.stop }
-end
